0f56fdb1b20234b0df8a072a20ccae7c1ea60292
REF office <kaitodotwav@gmail.com>
2024-03-11 16:29:32 +0800

    small change

--- main.py
+++ main.py
@@ -13,13 +13,19 @@ def check(item):
     return save
 n = input("name>> ")
 collected = []
+end = 0
 for row in dataframe1.iter_rows(0, dataframe1.max_row):
     name = row[1].value
     c = fuzz.token_sort_ratio(name, n)
     if c > 55:
         collected.append(f"{c} : {name}")
+    if name == None:
+        end += 1
+
+end_of_line = dataframe1.max_row - end
+
 for i in range(len(collected)):
     print(sorted(collected, reverse=True, key=lambda x: int(x.split(":")[0]))[i])
 
-print(dataframe1.max_row)
+#write new
 
